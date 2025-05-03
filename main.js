5a5401cf30c1630cd4c6f3d6654b83b22fea2d54
ifaixal <fai.iqbal124@gmail.com>
2025-05-03 14:26:45 +0500

    Day 4 Task done, Beside that fixed a few places

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-import FullNode from "./Core/Classes/FullNode";
-import provideInterface from "./Core/Network/JSONRpc";
+import FullNode from "./Core/Classes/FullNode.js";      //Added .js to the end of the FullNode since prior to that it was making import error
+import provideInterface from "./Core/Network/JSONRpc.js";
 
 
-let Node = FullNode();
+let Node = new FullNode();  //Added new prior to FullNode() to ensure it run's perfectly, Syntax Error
 
 provideInterface(Node);
-
