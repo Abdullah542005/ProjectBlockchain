6e0264b5d7cd12af2fff2793b3211893378770e6
ifaixal <fai.iqbal124@gmail.com>
2025-05-06 00:03:25 +0500

    toJsonDB & fromJsonDB f(n)'s has been created - Task 6+7

--- Core/Database/DatabaseConnector.js
+++ Core/Database/DatabaseConnector.js
@@ -1,5 +1,40 @@
 import Database from "better-sqlite3";
+
+let db;
+
 export default function connectDataBase(){
-      const db = new Database("Core/Database/Blockchain.db");
+      db = new Database("Core/Database/Blockchain.db");
       return db;
 }
+
+function toJsonDB(blockNumber)
+{
+      let lastBlockNumber = db.prepare('Select max(blockNumber) As number from block').get();
+      lastBlockNumber = lastBlockNumber.number;       //Since the query returns an Object {number : xyz} so .number convert it
+      
+      let results = []
+      
+      for (let i = blockNumber; i <= lastBlockNumber; i++)
+      {
+            let block = db.prepare('select * from Block where blockNumber = ?').get(i);
+            let blockTransaction = db.prepare('select * from Transactions where blockhash = ?').get(block.currentblockhash);
+            const BlockData = {BlockHeader: block, Transactions: blockTransaction};
+            results.push(BlockData);            //Test can remove
+      }
+      console.log(results);
+      return results;
+}
+
+function fromJsonDB(Node, data)           //Yet to be tested
+{
+      for (let i = 0; i < data.length; i++)
+      {
+            let block = data[i].BlockHeader;
+            let transaction = data[i].Transactions;
+            Node.createMinedBlock(block, transaction);
+      }
+}
+
+//Testing portion can remove
+connectDataBase();
+toJsonDB(284);
